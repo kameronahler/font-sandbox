0b146c3da62f766b7946d98331e963fdaf3c1c40
Kameron Ahler <kamahler@gmail.com>
2018-11-22 13:03:48 -0700

    add initial files

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+// npm install --save-dev gulp, gulp-sass, gulp-postcss, autoprefixer, postcss-pxtorem, cssnano, gulp-uglify, gulp-concat, browser-sync
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    postcssautoprefixer = require('autoprefixer'),
+    postcsspxtorem = require('postcss-pxtorem'),
+    postcsscssnano = require('cssnano'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync')
+
+gulp.task('sass', function() {
+    var plugins = [
+        postcssautoprefixer({
+                browsers: ['last 4 versions', '> .25% in US']
+            }),
+        postcsspxtorem({
+            rootValue:16,
+            unitPrecision:5,
+            propList:['*'],
+            replace:false
+            }),
+        postcsscssnano({
+            calc: false,
+            colorMin: false,
+            convertValues: false,
+            discardUnused: false,
+            zindex: false,
+            reduceIdents: false,
+            mergeIdents: false,
+            minifySelectors: false,
+            minifyFontValues: false,
+            normalizeUrl: false,
+            safe: true,
+            mergeRules: true
+            })
+    ];
+
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+
+gulp.task('js', function() {
+    return gulp.src(['src/js/test.js','js/**/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('watch', ['sass', 'js', 'browserSync'], function() {
+    gulp.watch('src/**/*.scss', ['sass']);
+    gulp.watch('**/*.html', browserSync.reload);
+    gulp.watch('src/**/*.js', ['js']);
+});
+
+gulp.task('default', function() {
+    gulp.start('sass');
+    gulp.start('js');
+});
