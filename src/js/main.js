9b31209b7d814f47490da0760c14fc13bf115170
Kameron Ahler <kamahler@gmail.com>
2018-11-23 12:14:17 -0700

    add more fonts, paragraph example, clean class names

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var	formHeadingFont = document.querySelector('#font-picker-headings'),
-		formHeadingWeight = document.querySelector('#weight-picker-headings'),
-		formLabelFont = document.querySelector('#font-picker-labels'),
-		formLabelWeight = document.querySelector('#weight-picker-labels'),
-		formBodyFont = document.querySelector('#font-picker-body'),
-		formBodyWeight = document.querySelector('#weight-picker-body');
+	var	formHeadingFont = document.querySelector('#option-picker-headings'),
+		formHeadingWeight = document.querySelector('#weight-headings'),
+		formLabelFont = document.querySelector('#option-picker-labels'),
+		formLabelWeight = document.querySelector('#weight-labels'),
+		formBodyFont = document.querySelector('#option-picker-body'),
+		formBodyWeight = document.querySelector('#weight-body');
 	var currentHeadingFont,
 		currentHeadingWeight,
 		currentLabelFont,
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
    	// headings font family
    	formHeadingFont.addEventListener('click', function(e){
 		window.setTimeout(function(){
-			currentHeadingFont = document.querySelector('input[name="font-picker-headings"]:checked').value;
+			currentHeadingFont = document.querySelector('input[name="font-headings"]:checked').value;
 			document.documentElement.style.setProperty('--font-family-headings', currentHeadingFont);
 		}, 100
 		)
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
    	// labels font family
    	formLabelFont.addEventListener('click', function(e){
 		window.setTimeout(function(){
-			currentLabelFont = document.querySelector('input[name="font-picker-labels"]:checked').value;
+			currentLabelFont = document.querySelector('input[name="font-labels"]:checked').value;
 			document.documentElement.style.setProperty('--font-family-labels', currentLabelFont);
 		}, 100
 		)
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
    	// body font family
    	formBodyFont.addEventListener('click', function(e){
 		window.setTimeout(function(){
-			currentBodyFont = document.querySelector('input[name="font-picker-body"]:checked').value;
+			currentBodyFont = document.querySelector('input[name="font-body"]:checked').value;
 			document.documentElement.style.setProperty('--font-family-body', currentBodyFont);
 		}, 100
 		)
